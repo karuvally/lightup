9c47ac3533a60960631ed2d8721a02148c7168b9
Aswin Karuvally <aswinbabuk@gmail.com>
2018-02-13 12:29:11 +0530

    brightness range from 0 to 100

--- lightup.py
+++ lightup.py
@@ -9,28 +9,42 @@ import os
 import shutil
 import argparse
 
+# todo
+# exception if set value is greater than 100
+# if current_brightness + increment_value > max_brightness, set max_brightness
 
 # set or get the brightness
-def brightness(brightness_file_path, function, value):
+def brightness(brightness_directory, function, value):
     # get the current brightness value
-    with open(brightness_file_path, 'r') as brightness_file:
+    with open(brightness_directory + '/brightness', 'r') as brightness_file:
         brightness_value = brightness_file.readline().rstrip()
 
+    # get the maximum brightness value
+    with open(brightness_directory + '/max_brightness', 'r') as max_brightness:
+        max_brightness_value = max_brightness.readline().rstrip()
+
+    # calculate the step value
+    step_value = int(int(max_brightness_value) / 100)
+    
     if function == 'get':
-        print("current brightness is " + brightness_value)
+        print("current brightness is " +
+        str(int(brightness_value) / step_value))
         exit()
 
     # keep the file opened for rest of the operations
-    brightness_file = open(brightness_file_path, 'w')
+    brightness_file = open(brightness_directory + '/brightness', 'w')
 
     if function == 'set':
-        brightness_file.write(value)
+        brightness_file.write(str(value * step_value))
 
+    # debug! convert value to int
     if function == 'increment':
-        brightness_file.write(str(int(brightness_value) + int(value)))
+        brightness_file.write(str(int(brightness_value) +
+        int(value) * step_value))
         
     elif function == 'decrement':
-        brightness_file.write(str(int(brightness_value) - int(value)))
+        brightness_file.write(str(int(brightness_value) -
+        int(value) * step_value))
         
     # close the file
     brightness_file.close()
@@ -39,7 +53,7 @@ def brightness(brightness_file_path, function, value):
 # the main function
 def main():
     # set some essential variables
-    brightness_file_path = '/sys/class/backlight/intel_backlight/brightness'
+    brightness_directory = '/sys/class/backlight/intel_backlight'
     
     # parse run-time arguments
     parser = argparse.ArgumentParser(description=
@@ -58,19 +72,19 @@ def main():
     
     # set brightness using the -b argument
     if arguments.brightness:
-        brightness(brightness_file_path, 'set', arguments.brightness)
+        brightness(brightness_directory, 'set', int(arguments.brightness))
 
     # increase brightness using the -i argument
     elif arguments.increment:
-        brightness(brightness_file_path, 'increment', arguments.increment)
+        brightness(brightness_directory, 'increment', int(arguments.increment))
 
     # decrease brightness using the -d argument
     elif arguments.decrement:
-        brightness(brightness_file_path, 'decrement', arguments.decrement)
+        brightness(brightness_directory, 'decrement', int(arguments.decrement))
         
     # return current brightness
     else:
-        brightness(brightness_file_path, 'get', None)
+        brightness(brightness_directory, 'get', None)
 
 
 # run the main function
